4d84e86baaffb56f744cec0114bfa5768690b391
dianasharafutdinova <d.e.sharafutdinova@gmail.com>
2023-05-16 20:32:43 -0400

    Initial commit

--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -1,8 +1,11 @@
 package tests;
 
+import javax.xml.bind.SchemaOutputResolver;
+
 public class Test1 {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+        System.out.println("Java");
     }
 }
